d4f1ee17eff798aff795e37365cbc59e2585ec47
Tprezioso <tommyprezioso@gmail.com>
2019-04-01 10:45:35 -0400

    added auto resizing of label

--- Parks Events/Controller/DetailTableViewController.swift
+++ Parks Events/Controller/DetailTableViewController.swift
@@ -8,20 +8,20 @@
 
 import UIKit
 
+class detailCell: UITableViewCell {
+    @IBOutlet weak var detailLabel: UILabel!
+
+}
+
 class DetailTableViewController: UITableViewController {
 
     var detailEventArray = [String : String]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(">>>>>>>>>>>>>>>>>>>>>>>> \(detailEventArray)")
         tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = UITableView.automaticDimension
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.estimatedRowHeight = 600
     }
 
     // MARK: - Table view data source
@@ -32,8 +32,8 @@ class DetailTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "detailEventCell", for: indexPath)
-        cell.textLabel?.text = Array(self.detailEventArray.values)[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailEventCell", for: indexPath) as! detailCell
+        cell.detailLabel?.text = Array(self.detailEventArray.values)[indexPath.row]
 
         // Configure the cell...
 
@@ -43,50 +43,5 @@ class DetailTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
